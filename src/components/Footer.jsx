50248b9b6b243061ae8581adda70513898d0990c
Alexey Prosvirnin <aprosv@ya.ru>
2022-04-18 19:06:08 +0300

    updated links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,19 @@ function Footer() {
     // const inst=
   return (
     <div className={styles.container}>
-        <div className={styles.col}>
-            <div>Телефон<br/> <a href='tel:+79959018119'>+7 (995) 901-8119</a></div>
-            <div>E-mail <br/><a href="mailto:info@aizem.ru?subject=Давайте%20сделаем%20крутой%20проект">info@aizem.ru</a></div>
-        </div>
-        <div className={styles.col}>
+
+            <div className={styles.col}>Телефон<br/> <a href='tel:+79959018119'>+7 (995) 901-8119</a></div>
+            <div className={styles.col}>E-mail <br/><a href="mailto:info@aizem.ru?subject=Давайте%20сделаем%20крутой%20проект">info@aizem.ru</a></div>
+            <div className={styles.col}>Follow us <br/><a href="https://www.instagram.com/aizemverse/"  target="_blank">@aizemverse</a></div>
+            <div className={styles.col}>Behance <br/><a href="https://www.behance.net/aizemmoscow"  target="_blank">@aizemmoscow</a></div>
+            <div className={styles.col}>Telegram <br/><a href="https://t.me/aizemverse" target="_blank">@aizemverse</a></div>
+
+        {/* <div className={styles.col}> */}
         {/* <FontAwesomeIcon icon={faInstagram} size="2x" color='#333333'/> */}
-        <FontAwesomeIcon icon={faBehance} size="2x" color='#333333'/>
+        {/* <FontAwesomeIcon icon={faBehance} size="2x" color='#333333'/>
         <FontAwesomeIcon icon={faTelegram} size="2x" color='#333333'/>
-        <FontAwesomeIcon icon={faDribbble} size="2x" color='#333333'/>
-        </div>
+        <FontAwesomeIcon icon={faDribbble} size="2x" color='#333333'/> */}
+        {/* </div> */}
 
     </div>
   )
